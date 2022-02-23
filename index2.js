bc0d4582a988c7ff046a6972be0bde6f36677aef
AvaCado <eliserichardsson@gmail.com>
2022-02-23 14:26:14 -0500

    catch uncommented and code linted

--- index2.js
+++ index2.js
@@ -1,11 +1,5 @@
 const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-// fetchMyIP()
-//   .then(fetchCoordsByIP)
-//   .then(fetchISSFlyOverTimes)
-//   .then(body => console.log(JSON.parse(body).response));
-
-
   nextISSTimesForMyLocation()
   .then((passTimes) => {
     passTimes.forEach(({ risetime, duration }) => 
@@ -19,6 +13,6 @@ const { nextISSTimesForMyLocation } = require('./iss_promised');
     );
     
   })
-  // .catch((error) => {
-  //   console.log("It didn't work: ", error.message);
-  // });
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
